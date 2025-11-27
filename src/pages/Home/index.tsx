5d6deff698b766cfad3caadcac6651f9b15ee3c9
Kaua-cel <kauaneres.dev@gmail.com>
2025-11-27 16:29:00 -0300

    feat: header creation

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import geopes from '/src/assets/geopes-logo.png'
+import './style.css'
+
+function Home() {
+  return (
+    <>
+      <header className="flex items-center justify-between px-8 bg-[#868993] text-white">
+        <div className="flex items-center gap-1">
+          <img className="size-16" src={geopes} alt="logo GeoPes" />
+          <p className="text-1xl">GeoPes</p>
+        </div>
+        <p className="text-1xl">
+          <a href="#">Sobre</a>
+        </p>
+      </header>
+    </>
+  )
+}
+
+export default Home

1a664c5d3989cbc0df0f5aea992d734010c0f7c0
Kaua-cel <kauaneres.dev@gmail.com>
2025-11-27 19:23:33 -0300

    style: styling of country cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import './style.css'
 import { FaSearch } from 'react-icons/fa'
 import { FaSlidersH } from 'react-icons/fa'
 import { FaHeart } from 'react-icons/fa'
+import { FaRegHeart } from 'react-icons/fa'
 
 function Home() {
   return (
@@ -26,18 +27,57 @@ function Home() {
           <FaSlidersH />
         </div>
 
-        <section>
-          <div>
-            <div>
-              <img
-                className="h-auto w-24"
-                src="/src/assets/brasil.jpg"
-                alt="#"
-              />
-              <h2>País</h2>
+        <section className="w-full flex flex-col items-center gap-4 p-4">
+          <div className="bg-white shadow-md w-5/6 h-2/3 p-6 rounded-sm  sm:p-8 md:w-4/6 md:p-10 lg:w-3/6">
+            <div className="flex justify-between items-start">
+              <div className="flex gap-3">
+                <img
+                  className="h-auto w-24 sm:w-28 md:w-32"
+                  src="/src/assets/brasil.jpg"
+                  alt="#"
+                />
+                <h2 className="text-2xl font-bold">País</h2>
+              </div>
+              <FaHeart className="text-2xl text-[#194167]" />
             </div>
 
-            <FaHeart />
+            <div>
+              <p className="font-bold">
+                Capital: <span className="font-normal">Brasilia</span>
+              </p>
+              <p className="font-bold">
+                Região: <span className="font-normal">Américas</span>
+              </p>
+              <p className="font-bold">
+                População: <span className="font-normal">214,3 M</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="bg-white shadow-md w-5/6 h-2/3 p-6 rounded-sm  sm:p-8 md:w-4/6 md:p-10 lg:w-3/6">
+            <div className="flex justify-between items-start">
+              <div className="flex gap-3">
+                <img
+                  className="h-auto w-24 sm:w-28 md:w-32"
+                  src="/src/assets/brasil.jpg"
+                  alt="#"
+                />
+                <h2 className="text-2xl font-bold">País</h2>
+              </div>
+              <FaRegHeart className="text-2xl text-[#194167]" />
+            </div>
+
+            <div>
+              <p className="font-bold">
+                Capital: <span className="font-normal">Brasilia</span>
+              </p>
+              <p className="font-bold">
+                Região: <span className="font-normal">Américas</span>
+              </p>
+              <p className="font-bold">
+                População: <span className="font-normal">214,3 M</span>
+              </p>
+            </div>
           </div>
         </section>
       </main>

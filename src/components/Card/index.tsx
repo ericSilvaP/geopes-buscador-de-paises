9d3410760b868cb8a973907c34658f2b21951f1b
Eric Silva <ericsilvap13@gmail.com>
2025-11-28 12:52:41 -0300

    style: home page grid coutries cards; feat: show n countries cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,18 +1,21 @@
 import { FaHeart } from 'react-icons/fa'
+import './style.css'
 
 function Card() {
   return (
-    <div className="bg-white shadow-md w-5/6 h-2/3 p-6 rounded-sm  sm:p-8 md:w-4/6 md:p-10 lg:w-3/6">
+    <div className="bg-white shadow-md min-w-5/6 min-h-2/3 p-6 rounded-sm sm:p-8 md:min-w-4/6 md:p-10 lg:min-w-3/6 country-card">
       <div className="flex justify-between items-start">
-        <div className="flex gap-3">
+        <div className="flex gap-2 max-w-fit md:flex-col pb-3">
           <img
             className="h-auto w-24 sm:w-28 md:w-32"
             src="/src/assets/brasil.jpg"
             alt="#"
           />
-          <h2 className="text-2xl font-bold">País</h2>
+          <h2 className="text-2xl font-bold max-w-2/3 md:max-w-full">
+            República Democárica do Congo
+          </h2>
+          <FaHeart className="text-2xl text-[#194167]" />
         </div>
-        <FaHeart className="text-2xl text-[#194167]" />
       </div>
 
       <div>

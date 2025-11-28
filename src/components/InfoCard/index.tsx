b9bdb32d5e52856219da3563e12bc11e5e648bea
Kaua-cel <kauaneres.dev@gmail.com>
2025-11-28 09:56:29 -0300

    style: infoCard styling

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -17,10 +17,10 @@ const iconMap: Record<TypeMiniCard, React.ReactNode> = {
 
 function InfoCard({ typeIcon, typeInformation, description }: MiniCardProps) {
   return (
-    <div>
-      <div>
-        <p>{typeInformation}</p>
-        {iconMap[typeIcon]}
+    <div className="flex flex-col gap-1 bg-[#C0E0FF] p-4  rounded-sm text-[#194167]">
+      <div className="flex flex-row justify-between items-center">
+        <p className="font-bold">{typeInformation}</p>
+        <p className="text-xl sm:text-2xl ">{iconMap[typeIcon]}</p>
       </div>
 
       <p>{description}</p>

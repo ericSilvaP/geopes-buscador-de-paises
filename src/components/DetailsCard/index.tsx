620eb3f50a8b229a3f7924e7806f3a50d38c0501
Kaua-cel <kauaneres.dev@gmail.com>
2025-11-28 08:45:04 -0300

    feat: structuring the DetailsCard component

--- src/components/DetailsCard/index.tsx
+++ src/components/DetailsCard/index.tsx
@@ -1,29 +1,25 @@
-import { FaHeart } from 'react-icons/fa'
+// import { FaHeart } from 'react-icons/fa'
 
 function DetailsCard() {
   return (
-    <div className="bg-white shadow-md w-5/6 h-2/3 p-6 rounded-sm  sm:p-8 md:w-4/6 md:p-10 lg:w-3/6">
-      <div className="flex justify-between items-start">
-        <div className="flex gap-3">
-          <img
-            className="h-auto w-24 sm:w-28 md:w-32"
-            src="/src/assets/brasil.jpg"
-            alt="#"
-          />
-          <h2 className="text-2xl font-bold">País</h2>
-        </div>
-        <FaHeart className="text-2xl text-[#194167]" />
-      </div>
+    <div className="bg-white shadow-md w-full max-w-xl p-6 rounded-sm sm:p-8">
+      <h1 className="text-3xl font-bold">País</h1>
+      <img
+        className="w-32 sm:w-40 md:w-48 lg:w-56 object-cover rounded-sm"
+        src="/src/assets/brasil.jpg"
+        alt="#"
+      />
+      <p>Nome Completo do País</p>
 
-      <div>
+      <div className="flex gap-8">
         <p className="font-bold">
-          Capital: <span className="font-normal">Brasilia</span>
+          Capital <span className="font-normal">Brasilia</span>
         </p>
         <p className="font-bold">
-          Região: <span className="font-normal">Américas</span>
+          Região <span className="font-normal">Américas</span>
         </p>
         <p className="font-bold">
-          População: <span className="font-normal">214,3 M</span>
+          População <span className="font-normal">214,3 M</span>
         </p>
       </div>
     </div>

fab16d57d21be42d1fd44041d041198b1e03aa18
Eric Silva <ericsilvap13@gmail.com>
2025-12-02 10:26:35 -0300

    fix: dotenv not used

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import dotenv from 'dotenv'
 
 // https://vite.dev/config/
 export default defineConfig({

f5e99f5d92bae56a6095b4f2cfc47e55a4c9a5a7
Eric Silva <ericsilvap13@gmail.com>
2025-12-01 22:40:47 -0300

    build: dotenv; google maps api key in .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   // base: process.env.VITE_BASE_PATH || '/geopes-buscador-de-paises',
   base: process.env.VITE_BASE_PATH,
+  define: {
+    'process.env.GOOGLE_MAPS_API_KEY': JSON.stringify(
+      process.env.VITE_GOOGLE_MAPS_API_KEY
+    ),
+  },
 })
